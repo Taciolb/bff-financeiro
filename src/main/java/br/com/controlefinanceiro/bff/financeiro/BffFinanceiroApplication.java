878acffe336ec07517a71e099ce79d907c54e8d0
Taciano <taciolb@gmail.com>
2026-06-20 20:15:49 -0300

    feat: inicializado projeto bff-financeiro

---
+++ src/main/java/br/com/controlefinanceiro/bff/financeiro/BffFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.bff.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controlefinanceiro/bff/financeiro/BffFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.bff.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e56383a045644ba6868f12e3a254185ec3415954
Taciano <taciolb@gmail.com>
2026-06-21 10:11:32 -0300

    feat: add EnableFeignClients na BffFinanceiroApplication

--- src/main/java/br/com/controlefinanceiro/bff/financeiro/BffFinanceiroApplication.java
+++ src/main/java/br/com/controlefinanceiro/bff/financeiro/BffFinanceiroApplication.java
@@ -2,11 +2,14 @@ package br.com.controlefinanceiro.bff.financeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BffFinanceiroApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BffFinanceiroApplication.class, args);
 	}
 
